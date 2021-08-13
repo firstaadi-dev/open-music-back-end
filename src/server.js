e11a098cffbf93c3e5c0afa8ba5873c123f2b915
Firsta Adi Pradana <firsta@pop-os.localdomain>
2021-08-13 14:31:47 +0700

    firsta commit with server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
